2b025dff4784f85a2ae795130b39da25b1bdfae3
Georgy Akopov <akopovzh@yandex.ru>
2023-11-18 21:59:56 +0300

    crash-test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,7 +9,15 @@ const users = require('./users');
 const cards = require('./cards');
 const { validationCreateUser, validationLog } = require('../middlewares/validation');
 const NotFoundError = require('../errors/NotFoundError');
-
+// 0|app    | Error: Сервер сейчас упадёт
+// 0|app    |     at Timeout._onTimeout (/home/zhoraakop/react-mesto-api-f
+// ull-gha/backend/routes/index.js:15:11)
+// 0|app    |     at listOnTimeout (node:internal/timers:569:17)
+// 0|app    |     at process.processTimers (node:internal/timers:512:7)
+// PM2      | App [app:0] exited with code [1] via signal [SIGINT]
+// PM2      | App [app:0] starting in -fork mode-
+// PM2      | App [app:0] online
+// 0|app    | CONNECTED TO MONGODB
 router.get((process.env.API ? (`/${process.env.API}/crash-test`) : ('/crash-test')), () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
