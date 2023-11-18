d822c322edfcd11337cc116841addad5ccf45e5d
Georgy Akopov <akopovzh@yandex.ru>
2023-11-18 17:12:48 +0300

    full 15 project

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const auth = require('../middlewares/auth');
 
+const { API = '' } = process.env;
+
 const { login, createUser } = require('../controllers/users');
 const users = require('./users');
 const cards = require('./cards');
@@ -14,12 +16,12 @@ router.get('/crash-test', () => {
   }, 0);
 });
 
-router.post('/api/signup', validationCreateUser, createUser);
-router.post('/api/signin', validationLog, login);
+router.post(`/${API}signup`, validationCreateUser, createUser);
+router.post(`/${API}signin`, validationLog, login);
 
 router.use(auth);
-router.use('/api/users', users);
-router.use('/api/cards', cards);
+router.use(`/${API}users`, users);
+router.use(`/${API}cards`, cards);
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
