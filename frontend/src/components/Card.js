2f31e726c63dbc27327293e3cf8a43a1a6cc12b4
Georgy Akopov <akopovzh@yandex.ru>
2023-11-18 15:05:28 +0300

    vm2

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -16,7 +16,10 @@ const Card = ({ card, onCardClick, currentUser, onCardLike, onCardDelete }) => {
   console.log("card.owner._id", card.owner);
   console.log("currentUser._id", currentUser._id);
   
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => {
+    console.log("i_id" ,i._id)
+    return i._id === currentUser._id});
+  console.log("isliked", isLiked);
   const cardLikeButtonClassName = `element__like ${
     isLiked && "element__like_active"
   }`;
